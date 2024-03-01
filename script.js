d73019ea678e44ee06f318ccd6b98feffd239667
jveliquette <juliaveliquette@yahoo.com>
2024-02-29 16:31:09 -0800

    top bar styling and removal...added js and structure

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+'use strict';
 
+/* Preload */
+/* Loading will end after document is loaded */
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+})
