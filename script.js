d2725b70ff92dfc966d09be36cdbf3b83fa4f119
jveliquette <juliaveliquette@yahoo.com>
2024-02-29 21:25:02 -0800

    added styling to hero section, added responsive styling

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ window.addEventListener("load", function() {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
+
+// This function toggles the styling of my hamburger icon. The "open" class is used in CSS to control the appearance of the hamburger menu.
+// Calling this function switches between the open and closed states of the menu and icon.
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
